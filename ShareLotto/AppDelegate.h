4325ec2ffa2ec7e3f34e490e3ce0011f422dad51
MAC I <imac@IMACui-iMac.local>
2017-06-15 16:04:33 +0900

    Initial Commit

---
+++ ShareLotto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareLotto
+//
+//  Created by IMAC on 2017. 6. 15..
+//  Copyright © 2017년 IMAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShareLotto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShareLotto
+//
+//  Created by IMAC on 2017. 6. 15..
+//  Copyright © 2017년 IMAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
